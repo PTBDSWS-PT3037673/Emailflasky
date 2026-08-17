7586998378d7cedf0338c53005867be1f34f647d
Josh <joshua.m@aluno.ifsp.edu.br>
2026-08-17 19:14:22 +0000

    Aula 050 Formulários

--- hello.py
+++ hello.py
@@ -1,14 +1,21 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, flash, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
-
-from datetime import datetime
-
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = 'secret'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -19,26 +26,14 @@ def page_not_found(e):
 def internal_server_error(e):
     return render_template('500.html'), 500
 
-@app.route('/')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-	return render_template('index.html', current_time=datetime.utcnow(), name='Joshua Merces')
-
-
-@app.route('/user/<name>')
-def user(name):
-    return render_template('user.html', name=name)
-
-
-@app.route('/contextorequisicao/<name>')
-def contextorequisicao(name):
-    user_agent = request.headers.get('User-Agent')
-    remote_ip = request.remote_addr
-    host = request.host
-
-    return render_template(
-        'contextorequisicao.html',
-        name=name,
-        user_agent=user_agent,
-        remote_ip=remote_ip,
-        host=host
-    )
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
