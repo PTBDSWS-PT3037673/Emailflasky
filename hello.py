583aff6f53ce44c9113c49f26447c21c59370caf
lucasmoura123494 <moura.rodrigues@aluno.ifsp.edu.br>
2026-09-22 03:27:53 +0000

    Configura envio de email com SendGrid

--- hello.py
+++ hello.py
@@ -1,18 +1,21 @@
+
 import os
+import requests as http_requests
 from flask import Flask, render_template, session, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
-from wtforms import StringField, SelectField, SubmitField
+from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
-app.config['SQLALCHEMY_DATABASE_URI'] = \
+app.config['SQLALCHEMY_DATABASE_URI'] =\
     'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
@@ -21,18 +24,66 @@ moment = Moment(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+def enviar_email(nome_usuario):
+    """Envia e-mail via SendGrid API para o professor e para o aluno."""
+    try:
+        response = http_requests.post(
+            'https://api.sendgrid.com/v3/mail/send',
+            headers={
+                'Authorization': f'Bearer {SENDGRID_API_KEY}',
+                'Content-Type': 'application/json'
+            },
+            json={
+                'personalizations': [
+                    {
+                        'to': [{'email': email} for email in EMAIL_DESTINATARIOS]
+                    }
+                ],
+                'from': {
+                    'email': EMAIL_REMETENTE,
+                    'name': 'Flasky App'
+                },
+                'subject': f'Novo usuário cadastrado - {nome_usuario}',
+                'content': [
+                    {
+                        'type': 'text/html',
+                        'value': f'''
+                            <h2>Novo usuário cadastrado no Flasky</h2>
+                            <p><strong>Prontuário:</strong> {PRONTUARIO}</p>
+                            <p><strong>Nome do aluno:</strong> {NOME_ALUNO}</p>
+                            <hr>
+                            <p><strong>Usuário cadastrado:</strong> {nome_usuario}</p>
+                        '''
+                    }
+                ]
+            }
+        )
+        print(f'E-mail enviado! Status: {response.status_code} - {response.text}')
+        return response.status_code == 202
+    except Exception as e:
+        print(f'Erro ao enviar e-mail: {e}')
+        return False
+
+
+SENDGRID_API_KEY = os.environ.get('SENDGRID_API_KEY', 'COLE_SUA_CHAVE_AQUI')
+
+# E-mail verificado no SendGrid (Single Sender)
+EMAIL_REMETENTE = 'moura.rodrigues@aluno.ifsp.edu.br'
+
+EMAIL_DESTINATARIOS = [
+    'flaskaulasweb@zohomail.com',
+    'moura.rodrigues@aluno.ifsp.edu.br'
+]
+
+PRONTUARIO = 'PT3037673'
+NOME_ALUNO ='LucasRodrigues de moura'
+
 
 class Role(db.Model):
     __tablename__ = 'roles'
-
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
-
-    users = db.relationship(
-        'User',
-        backref='role',
-        lazy='dynamic'
-    )
+    users = db.relationship('User', backref='role', lazy='dynamic')
 
     def __repr__(self):
         return '<Role %r>' % self.name
@@ -40,7 +91,6 @@ class Role(db.Model):
 
 class User(db.Model):
     __tablename__ = 'users'
-
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
@@ -50,21 +100,7 @@ class User(db.Model):
 
 
 class NameForm(FlaskForm):
-    name = StringField(
-        'What is your name?',
-        validators=[DataRequired()]
-    )
-
-    role = SelectField(
-        'Role?:',
-        choices=[
-            ('Administrator', 'Administrator'),
-            ('Moderator', 'Moderator'),
-            ('User', 'User')
-        ],
-        validators=[DataRequired()]
-    )
-
+    name = StringField('What is your name?', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
 
@@ -85,36 +121,25 @@ def internal_server_error(e):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-
-    # Garante que as três funções existam no banco
-    roles_names = ['Administrator', 'Moderator', 'User']
-
-    for role_name in roles_names:
-        role = Role.query.filter_by(name=role_name).first()
-
-        if role is None:
-            role = Role(name=role_name)
-            db.session.add(role)
-
-    db.session.commit()
-
     form = NameForm()
 
     if form.validate_on_submit():
 
-        # Procura o usuário pelo nome
-        user = User.query.filter_by(
-            username=form.name.data
-        ).first()
 
-        # Procura a função escolhida
-        role = Role.query.filter_by(
-            name=form.role.data
-        ).first()
+        user = User.query.filter_by(username=form.name.data).first()
 
         if user is None:
 
-            # Cria o usuário com a função escolhida
+
+            role = Role.query.filter_by(name='User').first()
+
+
+            if role is None:
+                role = Role(name='User')
+                db.session.add(role)
+                db.session.commit()
+
+
             user = User(
                 username=form.name.data,
                 role=role
@@ -132,25 +157,13 @@ def index():
 
         return redirect(url_for('index'))
 
-    # Todos os usuários cadastrados
+
     users = User.query.all()
-
-    # Todas as funções cadastradas
-    roles = Role.query.all()
-
-    # Quantidade de usuários
-    users_count = User.query.count()
-
-    # Quantidade de funções
-    roles_count = Role.query.count()
 
     return render_template(
         'index.html',
         form=form,
         name=session.get('name'),
         known=session.get('known', False),
-        users=users,
-        roles=roles,
-        users_count=users_count,
-        roles_count=roles_count
+        users=users
     )
